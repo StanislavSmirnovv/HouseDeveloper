67bde4429c2ae21873fa1c6a3f2f93521224d42b
Smirnov Stanislavv <st.smirnov21@gmail.com>
2022-05-03 15:00:12 +0300

    Swiper Commit

--- js/common.js
+++ js/common.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+	$(".home-slider__heading").each(function () {
+		let text = $(this).text().split(" ");
+		let first = text.shift();
+		$(this).html(`${first} <br/><span>${text.join(" ")}</span>`);
+	});
 	// Menu
 	function toggleMenu() {
 		$(".menu-toggle").toggleClass("menu-toggle_active");
@@ -23,5 +28,27 @@ document.addEventListener("DOMContentLoaded", function () {
 		speed: 800,
 		effect: "fade",
 		centeredSlides: true,
+		pagination: {
+			el: ".home-slider__pagination",
+			type: "custom",
+			renderCustom: function (swiper, current, total) {
+				let indT = total >= 10 ? total : `0${total}`;
+				let indC = current >= 10 ? current : `0${current}`;
+				return `<b>${indC}</b> <span></span> ${indT}`;
+			},
+		},
+		scrollbar: {
+			el: ".home-slider__scrollbar",
+			draggable: true,
+		},
+		navigation: {
+			prevEl: ".home-slider__prev",
+			nextEl: ".home-slider__next",
+		},
+		keyboard: {
+			enable: true,
+			onlyInViewport: false,
+		},
+		runCallbacksOnInit: true,
 	});
 });
